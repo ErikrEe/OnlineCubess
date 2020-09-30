d869a8e48844a670b5cb82747132c74c41c025cf
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-09-30 09:38:20 +0200

    Cooood

---
+++ Assets/scripts/RoomController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    int SceneIndex;
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            print("starting game...");
+            PhotonNetwork.LoadLevel(SceneIndex);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
